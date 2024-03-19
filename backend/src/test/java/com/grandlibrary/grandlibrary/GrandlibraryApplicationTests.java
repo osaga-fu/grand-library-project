4f349e39ed9e2a455a01175c25d2bc293ef0c63c
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-03-19 17:15:08 +0100

    Created frontend an backend folders structure

---
+++ backend/src/test/java/com/grandlibrary/grandlibrary/GrandlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grandlibrary.grandlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
