ba2c7d51828d8c5b89916f757a508188cea8c457
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-03-24 15:40:25 +0100

    Created MemberService Backend

--- backend/src/main/java/com/grandlibrary/backend/controllers/members/MemberController.java
+++ backend/src/main/java/com/grandlibrary/backend/controllers/members/MemberController.java
@@ -1,19 +1,15 @@
 package com.grandlibrary.backend.controllers.members;
 
-
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.grandlibrary.backend.persistence.members.MemberRepository;
-import com.grandlibrary.backend.persistence.members.Member;
+import com.grandlibrary.backend.services.MemberService;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,42 +18,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 @CrossOrigin(origins = "http://localhost:5173")
 public class MemberController {
 
-    private MemberRepository respository;
-
-    public MemberController(@Autowired MemberRepository respository) {
-        this.respository = respository;
-    }
+    @Autowired
+    private MemberService memberService;
 
     @GetMapping("/members")
-    public List<Member> searchMembers(@RequestParam("query") String query) {
-        return respository
-                .findAllByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCaseOrDniContainingIgnoreCaseOrEmailContainingIgnoreCase(
-                        query, query, query, query);
+    public List<MemberResponse> searchMembers(@RequestParam("query") String query) {
+        return memberService.searchMembers(query);
     }
 
     @PostMapping("/members")
     public MemberResponse createMember(@RequestBody MemberRequest request) {
-        Member member = new Member(request.getMemberId(), request.getFirstName(), request.getLastName(),
-                request.getDni(), request.getEmail());
-        Member savedMember = respository.save(member);
-        return new MemberResponse(savedMember.getMemberId(), savedMember.getFirstName(),
-                savedMember.getLastName(), savedMember.getDni(), savedMember.getEmail());
-    }
-
-    @GetMapping("/members/{id}")
-    public ResponseEntity<MemberResponse> getMemberById(@PathVariable Long id) {
-        @SuppressWarnings("null")
-        Optional<com.grandlibrary.backend.persistence.members.Member> optionalMember = respository.findById(id);
-        if (optionalMember.isPresent()) {
-            Member existingMember = optionalMember.get();
-            MemberResponse memberResponse = new MemberResponse(existingMember.getMemberId(),
-                    existingMember.getFirstName(),
-                    existingMember.getLastName(), existingMember.getDni(), existingMember.getEmail());
-            return ResponseEntity.ok(memberResponse);
-
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return memberService.createMember(request);
     }
 
 }
--- backend/src/main/java/com/grandlibrary/backend/services/MemberService.java
+++ backend/src/main/java/com/grandlibrary/backend/services/MemberService.java
@@ -1,5 +1,42 @@
 package com.grandlibrary.backend.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.grandlibrary.backend.controllers.members.MemberRequest;
+import com.grandlibrary.backend.controllers.members.MemberResponse;
+import com.grandlibrary.backend.persistence.members.Member;
+import com.grandlibrary.backend.persistence.members.MemberRepository;
+
+@Service
 public class MemberService {
-    
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public List<MemberResponse> searchMembers(String query) {
+        List<Member> members = memberRepository
+                .findAllByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCaseOrDniContainingIgnoreCaseOrEmailContainingIgnoreCase(
+                        query, query, query, query);
+
+        List<MemberResponse> memberResponses = new ArrayList<>();
+        for (Member member : members) {
+            memberResponses.add(new MemberResponse(member.getMemberId(), member.getFirstName(),
+                    member.getLastName(), member.getDni(), member.getEmail()));
+        }
+
+        return memberResponses;
+    }
+
+    public MemberResponse createMember(MemberRequest request) {
+        Member member = new Member(request.getMemberId(), request.getFirstName(), request.getLastName(),
+                request.getDni(), request.getEmail());
+        Member savedMember = memberRepository.save(member);
+        return new MemberResponse(savedMember.getMemberId(), savedMember.getFirstName(),
+                savedMember.getLastName(), savedMember.getDni(), savedMember.getEmail());
+    }
+
 }
